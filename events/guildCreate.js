8bcbb5284e54be3f99a530d471ca563a074865dd
Fluxpuck - Mathijs <mathijsvanderkuijp@live.nl>
2023-05-01 21:33:22 +0200

    guild events

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,8 +1,20 @@
 /*  FluxBot © 2023 Fluxpuck
 This event is triggers by Discord and does processing of data  */
 
+// → Importing necessary modules, functions and classes
+const { postRequest } = require('../database/connection');
+
 module.exports = async (client, guild) => {
 
+    // Save guild to API
+    const response = await postRequest(`/guilds/${guild.id}`, {
+        guild: {
+            guildId: guild.id,
+            guildName: guild.name,
+        }
+    })
+
+    console.log(response.status, response.data)
 
     return;
 }
--- events/guildDelete.js
+++ events/guildDelete.js
@@ -3,6 +3,9 @@ This event is triggers by Discord and does processing of data  */
 
 module.exports = async (client, guild) => {
 
+    // Remove guild from API
+    const response = await deleteRequest(`/guilds/${guild.id}`);
+    console.log(response.status, response.data)
 
     return;
 }
