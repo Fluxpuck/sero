5d4ada7cdfde45f1e89b7e426e1ac2232e56eb1d
Mathijs <Mathijsvanderkuijp@live.nl>
2023-12-02 12:16:08 +0100

    Small Improvements

--- packages/bot/commands/moderation/disconnect.js
+++ packages/bot/commands/moderation/disconnect.js
@@ -1,8 +1,4 @@
-/*  Fluxpuck © Creative Commons Attribution-NoDerivatives 4.0 International Public License
-    For more information on the commands, please visit hyperbot.cc  */
-
-// -> Prepare the command parameters
-    module.exports.props = {
+module.exports.props = {
     commandName: "disconnect",
     description: "Disconnect a user from their voicechannel",
     usage: "/disconnect [user]",
@@ -12,27 +8,27 @@
             {
                 name: "user",
                 description: "Select a user to disconnect from a voicechannel",
-                type: 6, // → https://discord-api-types.dev/api/discord-api-types-v10/enum/ApplicationCommandOptionType (Type 6 for user)
-                required: true // Requires a user to be selected
+                type: 6,
+                required: true
             }
         ]
     }
 };
 
-// → Constructing the command and exporting
 module.exports.run = async (client, interaction) => {
-    const targetUser = interaction.options.get("user"); // Get the selected user from the interaction
-    const targetMember = await interaction.guild.members.fetch(targetUser.value); // Get the member object from the selected user for the current guild (value is the id of the given user)
+    const targetUser = interaction.options.get("user");
+    const targetMember = await interaction.guild.members.fetch(targetUser.value);
 
-    if (targetMember.voice.channel) { // Check if the member is in a voicechannel (returns channel or undefined, which is true or false respectively)
+    if (targetMember?.voice.channel) {
         await targetMember.voice.disconnect();
         return interaction.reply({
-            content: `Successfully disconnected <@${targetMember.id}> from the voicechannel`, 
-            ephemeral: true,
+            content: `Successfully disconnected <@${targetMember.id}>`,
+            ephemeral: false,
         });
     } else {
-        return interaction.reply({ 
-            content: `<@${targetMember.id}> is not in a voicechannel`, 
-            ephemeral: true });
+        return interaction.reply({
+            content: `<@${targetMember.id}> is not in a voicechannel`,
+            ephemeral: true
+        });
     }
 };
