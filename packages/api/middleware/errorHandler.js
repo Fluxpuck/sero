5ebb932c65b7d1ae0b48abee6ce91b7e1cd55c7e
Mathijs <Mathijsvanderkuijp@live.nl>
2023-12-11 17:55:16 +0100

    Improved API Error Handling

--- packages/api/middleware/errorHandler.js
+++ packages/api/middleware/errorHandler.js
@@ -1,13 +1,22 @@
 module.exports = {
 
     errorHandler(err, req, res, next) {
-        //set default error status code to 500 (Internal Server Error)
+        // Set default error status code to 500 (Internal Server Error)
         if (!err.status) err.status = 500;
 
-        //log the error to the console → for debugging purposes only!
-        console.error("[API ErrorHandler]:", err);
+        // Build the error response
+        const ErrorResponse = {
+            status: err.status,
+            message: err.message,
+            data: {
+                url: req.originalUrl,
+                method: req.method,
+                timestamp: new Date()
+            }
+        };
 
-        console.log(req.originalUrl)
+        // Log the error to the console → for debugging purposes only!
+        console.error("[API ErrorHandler]: ", ErrorResponse);
 
         // Send the error response to the client
         return res.status(err.status).json({
