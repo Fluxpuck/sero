68966d319eab44a6618a9677b7538d1f4a7b9884
Mathijs <Mathijsvanderkuijp@live.nl>
2023-12-12 01:12:56 +0100

    Small fix to Level EXP system

--- packages/bot/events/messageCreate.js
+++ packages/bot/events/messageCreate.js
@@ -11,12 +11,18 @@ module.exports = async (client, message) => {
      * If not, create/update User in the database
      */
     if (!message.author?.userHash) {
-        const result = await postRequest(`/users/${message.guildId}/${message.author.id}`, {
-            userName: message.author.username,
-        });
+        // Get User from database
+        const result = await getRequest(`/users/${message.guildId}/${message.author.id}`);
+        // If User is not in the Database, store it
+        if (result.status == 404) {
+            await postRequest(`/users/${message.guildId}/${message.author.id}`, {
+                userName: message.author.username,
+            });
+        }
 
         // Add the userHash to the User object
-        message.author.userHash = result.data?.userHash
+        message.author.userHash = result.data[0]?.userHash
+
     } else {
 
         // Store the messageId and channelId
@@ -25,42 +31,35 @@ module.exports = async (client, message) => {
             channelId: message.channelId
         });
 
+        console.log("Message stored")
 
+        /**
+         * This code will get a message per 60 seconds cooldown
+         * And will add experience to the user's level
+         */
+        const cooldownKey = message.author.userHash
+        if (client.cooldowns.has(cooldownKey) === false) {
 
+            // Check if user is present in Levels
+            const result = await getRequest(`/leaderboard/${message.guildId}/${message.author.id}`);
 
-    }
+            // If 404 error, create a new entry
+            if (result.status == 404) {
+                // Create a new entry in the leaderboard for the user and guild
+                const entry = await postRequest(`/leaderboard/${message.guildId}/${message.author.id}`);
+                // console.log(entry.data.message)
+            } else {
+                // Give the users experience
+                const gain = await postRequest(`/leaderboard/gain/${message.guildId}/${message.author.id}`);
+                // console.log(gain.data.message);
+            }
 
-
-
-
-
-
-
-
-    /**
-     * This code will get a message per 60 seconds cooldown
-     * And will add experience to the user's level
-     */
-    const cooldownKey = `${message.guildId}/${message.author.name}`
-    if (client.cooldowns.has(cooldownKey) === false) {
-
-        // Check if user is present in Levels
-        const result = await getRequest(`/leaderboard/${message.guildId}/${message.author.id}`);
-
-        // If 404 error, create a new entry
-        if (result.status == 404) {
-            // Create a new entry in the leaderboard for the user and guild
-            const entry = await postRequest(`/leaderboard/${message.guildId}/${message.author.id}`);
-            console.log("New Entry for Levels: " + entry);
-        } else {
-            // Give the users experience
-            const gain = await postRequest(`/leaderboard/gain/${message.guildId}/${message.author.id}`);
-            console.log("Update Entry for Levels:" + gain);
+            // Add the user to the cooldowns Collection
+            client.cooldowns.set(cooldownKey, message, 60)
         }
 
-        // Add the user to the cooldowns Collection
-        client.cooldowns.set(cooldownKey, message, 60)
     }
+
 
     return;
 }
