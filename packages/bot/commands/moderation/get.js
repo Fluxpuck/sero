41a77dc28d35582f3527115b31537bae9d28a262
fluxpuck <mathijsvanderkuijp@live.nl>
2023-12-25 14:19:13 +0100

    Update kick, created ban and initialized get
    - Get (rename of fetch command) will replace the search command

---
+++ packages/bot/commands/moderation/get.js
@@ -0,0 +1,26 @@
+module.exports.props = {
+    commandName: "get",
+    description: "Get user information and logs",
+    usage: "/get [user]",
+    interaction: {
+        type: 1,
+        options: [
+            {
+                name: "user",
+                description: "Select a user to get information on",
+                type: 6,
+                required: true,
+            },
+        ],
+    },
+};
+
+module.exports.run = async (client, interaction) => {
+    // Get User details from the interaction options
+    const targetUser = interaction.options.get("user").user;
+
+    
+
+
+
+}
