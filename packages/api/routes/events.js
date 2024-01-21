adb61d6f27f9095ae9b53a3c184d4a08cab4aac2
fluxpuck <mathijsvanderkuijp@live.nl>
2024-01-21 17:05:18 +0100

    Update models routing

--- packages/api/routes/events.js
+++ packages/api/routes/events.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { EventChannels } = require("../database/models");
+const { LoggingChannels } = require("../database/models");
 const { sequelize } = require('../database/sequelize');
 const { CreateError } = require('../utils/ClassManager');
 const { validateParams, validateData } = require('../utils/FunctionManager');
