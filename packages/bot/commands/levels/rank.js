6f3477221abca31f65c8bd538633d7b91b6c742c
Mathijs <Mathijsvanderkuijp@live.nl>
2024-01-30 00:45:19 +0100

    Added Cooldown to Rank command

--- packages/bot/commands/levels/rank.js
+++ packages/bot/commands/levels/rank.js
@@ -29,48 +29,63 @@ module.exports.run = async (client, interaction) => {
 		})
 	}
 
-	// Get the user experience
-	const result = await getRequest(`/levels/${interaction.guildId}/${targetUser.id}`);
+	/**
+	  * This code whas a 24 hour cooldown per user
+	  */
+	const cooldownKey = targetUser.id + interaction.id
+	if (client.cooldowns.has(cooldownKey) === false) {
 
-	// If status code is 404, return an error saying the user is not ranked yet
-	if (result.status === 404) {
+		// Get the user experience
+		const result = await getRequest(`/levels/${interaction.guildId}/${targetUser.id}`);
+
+		// If status code is 404, return an error saying the user is not ranked yet
+		if (result.status === 404) {
+			return interaction.reply({
+				content: `Uh oh! The user ${targetUser.username} is no rank yet!`,
+				ephemeral: true
+			})
+		} else if (result.status !== 200) { // If the status code is not 200, return an error that something went wrong
+			return interaction.reply({
+				content: "Oops! Something went wrong while trying to fetch the rank!",
+				ephemeral: true
+			})
+		}
+
+		// Get request details
+		const { userLevel, position } = result.data;
+		const { level, experience, nextLevelExp, currentLevelExp } = userLevel;
+
+		// Get the user's rank card
+		const rankCard = await createRankCard(
+			targetUser.id,
+			targetUser.username,
+			targetUser.displayAvatarURL({ forceStatic: true, extension: "png", size: 1024 }),
+			position,
+			experience,
+			level,
+			nextLevelExp,
+			currentLevelExp
+		);
+
+		// If creating the rank card was successful, return rankcard
+		if (rankCard) {
+			interaction.reply(
+				{ files: [rankCard] }
+			)
+		} else {
+			// Else return an error
+			interaction.reply({
+				content: "Oops! Something went wrong creating your rank card!",
+				ephemeral: true
+			})
+		}
+
+		// Add the user to the cooldowns Collection
+		return client.cooldowns.set(cooldownKey, interaction, 0 * 2 * 60) // 2 minutes
+	} else {
 		return interaction.reply({
-			content: `Uh oh! The user ${targetUser.username} is no rank yet!`,
-			ephemeral: true
-		})
-	} else if (result.status !== 200) { // If the status code is not 200, return an error that something went wrong
-		return interaction.reply({
-			content: "Oops! Something went wrong while trying to fetch the rank!",
+			content: `You can only transfer experience once per day!`,
 			ephemeral: true
 		})
 	}
-
-	// Get request details
-	const { userLevel, position } = result.data;
-	const { level, experience, nextLevelExp, currentLevelExp } = userLevel;
-
-	// Get the user's rank card
-	const rankCard = await createRankCard(
-		targetUser.id,
-		targetUser.username,
-		targetUser.displayAvatarURL({ forceStatic: true, extension: "png", size: 1024 }),
-		position,
-		experience,
-		level,
-		nextLevelExp,
-		currentLevelExp
-	);
-
-	// If creating the rank card was not successful, return an error
-	if (!rankCard) {
-		return interaction.reply({
-			content: "Oops! Something went wrong creating your rank card!",
-			ephemeral: true
-		})
-	}
-
-	// Return embed with rank details
-	return interaction.reply(
-		{ files: [rankCard] }
-	)
 }
