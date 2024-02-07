e2d89e1f873047cc9dac831474c1690938ac8044
Mathijs <Mathijsvanderkuijp@live.nl>
2024-02-07 01:23:17 +0100

    wip - leaderboard command

---
+++ packages/bot/commands/levels/leaderboard.js
@@ -0,0 +1,34 @@
+const { getRequest } = require("../../database/connection");
+
+module.exports.props = {
+    commandName: "leaderboard",
+    description: "Check the leaderboard for the guild",
+    usage: "/leaderboard",
+    interaction: {
+        type: 1,
+        options: [],
+    },
+    defaultMemberPermissions: [],
+}
+
+module.exports.run = async (client, interaction) => {
+
+    // Get all levels for a specific guild from the database
+    const result = await getRequest(`/levels/${interaction.guildId}`);
+
+    // If status code is 404, return an error
+    // if (result.status === 404) {
+    //     return interaction.reply({
+    //         content: `Uh oh! There are no users on the leaderboard yet!`,
+    //         ephemeral: true
+    //     })
+    // } else if (result.status !== 200) { // If the status code is not 200, return an error that something went wrong
+    //     return interaction.reply({
+    //         content: "Oops! Something went wrong while trying to fetch the leaderboard!",
+    //         ephemeral: true
+    //     })
+    // }
+
+    console.log(result)
+
+}
