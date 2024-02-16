6ea2347ab8dda2ba90049ccf51896b2e36fcd675
Mathijs <Mathijsvanderkuijp@live.nl>
2024-02-16 23:44:42 +0100

    Remove console.log from logchannel setup commands

--- packages/bot/commands/logging/remove-logchannel.js
+++ packages/bot/commands/logging/remove-logchannel.js
@@ -2,44 +2,43 @@ const { getRequest, deleteRequest } = require("../../database/connection")
 const { createCustomEmbed } = require("../../assets/embed")
 const { formatExpression } = require("../../lib/helpers/StringHelpers/stringHelper")
 const { LOGGING_CATEGORIES } = require("../../assets/logging")
-const {ERROR, WARNING, SUCCESS} = require("../../assets/embed-colors")
+const { ERROR, WARNING, SUCCESS } = require("../../assets/embed-colors")
 const { GetLogTypeByKeyOrValue } = require("../../config/LogTypes")
 
 module.exports.props = {
-    commandName: "remove-logchannel",
-    description: "Removes the saved log channel for the specified log type.",
-    usage: "/remove-logchannel [log-type]",
-    interaction: {
-        type: 1,
-        permissionType: [],
-        options: [
-            {
-                name: "log-type",
-                type: 3,
-                description: "Type of log to set the channel for.",
-                required: true,
+	commandName: "remove-logchannel",
+	description: "Removes the saved log channel for the specified log type.",
+	usage: "/remove-logchannel [log-type]",
+	interaction: {
+		type: 1,
+		permissionType: [],
+		options: [
+			{
+				name: "log-type",
+				type: 3,
+				description: "Type of log to set the channel for.",
+				required: true,
 				autocomplete: true,
 				maxLength: 100,
 				minLegnth: 4
-            }
-        ],
-    },
-    defaultMemberPermissions: ['ManageGuild'],
+			}
+		],
+	},
+	defaultMemberPermissions: ['ManageGuild'],
 }
 
 module.exports.autocomplete = async (client, interaction) => {
-    const focusedReason = interaction.options.getFocused();
+	const focusedReason = interaction.options.getFocused();
 
-    // Get and format the pre-reasons
-    const reasons = Object.keys(LOGGING_CATEGORIES).map(reason =>
-        ({ name: formatExpression(reason), value: LOGGING_CATEGORIES[reason] })
-    );
+	// Get and format the pre-reasons
+	const reasons = Object.keys(LOGGING_CATEGORIES).map(reason =>
+		({ name: formatExpression(reason), value: LOGGING_CATEGORIES[reason] })
+	);
 
-    // Get the focussed reason && return the filtered reason
-    const filteredReasons = reasons.filter(reason => reason.name.toLowerCase().includes(focusedReason.toLowerCase()));
-    interaction.respond(filteredReasons);
+	// Get the focussed reason && return the filtered reason
+	const filteredReasons = reasons.filter(reason => reason.name.toLowerCase().includes(focusedReason.toLowerCase()));
+	interaction.respond(filteredReasons);
 }
-
 
 module.exports.run = async (client, interaction) => {
 
@@ -48,14 +47,12 @@ module.exports.run = async (client, interaction) => {
 	const logTypeCategory = Object.keys(logObject)[0];
 	const logTypeCategoryName = Object.values(logObject)[0];
 
-	console.log(`The Interation option is:`, interaction.options.get("log-type").value)
-
 	// Send the request to check if the log channel exists
 	const logChannelResponse = await getRequest(`/logchannels/${interaction.guild.id}/${logTypeCategory}`);
 
 	// If there is no log channel for the logTypeCategory, create one
 	if (logChannelResponse.status === 404) {
-		
+
 		// Send an embed saying that the log channel for the logTypeCategory does not exist
 		sendEmbed(interaction, `No log channel`, `> There is no channel set for logging \`${logTypeCategoryName}\`.`, ERROR);
 
@@ -66,14 +63,14 @@ module.exports.run = async (client, interaction) => {
 
 		console.log(logChannelDeleteResponse)
 		// If the request was successful, return success message
-		if (logChannelDeleteResponse.status === 200) {			
+		if (logChannelDeleteResponse.status === 200) {
 			// Send the success embed
 			sendEmbed(interaction, `Log channel removed`, `> Successfully removed the log channel for \`${logTypeCategoryName}\`.`, SUCCESS);
 		} else {
 			// If the request was not successful, return an error
 			sendErrorResponse(interaction, `An error occurred while trying to delete the log channel for \`${logTypeCategoryName}\`.`);
 		}
-		
+
 	} else {
 		// If the request was not successful, return an error
 		sendEmbed(interaction, "Error", "An error occurred while deleting the log channel.", ERROR);
--- packages/bot/commands/logging/set-logchannel.js
+++ packages/bot/commands/logging/set-logchannel.js
@@ -2,51 +2,50 @@ const { getRequest, postRequest } = require("../../database/connection")
 const { createCustomEmbed } = require("../../assets/embed")
 const { formatExpression } = require("../../lib/helpers/StringHelpers/stringHelper")
 const { LOGGING_CATEGORIES } = require("../../assets/logging")
-const {ERROR, WARNING, SUCCESS} = require("../../assets/embed-colors")
+const { ERROR, WARNING, SUCCESS } = require("../../assets/embed-colors")
 const { GetLogTypeByKeyOrValue } = require("../../config/LogTypes")
 
 module.exports.props = {
-    commandName: "set-logchannel",
-    description: "Sets the channel for logging moderation and other actions",
-    usage: "/set-logchannel [log-type] [channel]",
-    interaction: {
-        type: 1,
-        permissionType: [],
-        options: [
-            {
-                name: "log-type",
-                type: 3,
-                description: "Type of log to set the channel for.",
-                required: true,
+	commandName: "set-logchannel",
+	description: "Sets the channel for logging moderation and other actions",
+	usage: "/set-logchannel [log-type] [channel]",
+	interaction: {
+		type: 1,
+		permissionType: [],
+		options: [
+			{
+				name: "log-type",
+				type: 3,
+				description: "Type of log to set the channel for.",
+				required: true,
 				autocomplete: true,
 				maxLength: 100,
 				minLegnth: 4
-            },
+			},
 			{
-                name: "channel",
-                type: 7,
-                description: "Channel to set to log actions in.",
-                required: true,
+				name: "channel",
+				type: 7,
+				description: "Channel to set to log actions in.",
+				required: true,
 				channelTypes: [0]
-            }
-        ],
-    },
-    defaultMemberPermissions: ['ManageGuild'],
+			}
+		],
+	},
+	defaultMemberPermissions: ['ManageGuild'],
 }
 
 module.exports.autocomplete = async (client, interaction) => {
-    const focusedReason = interaction.options.getFocused();
+	const focusedReason = interaction.options.getFocused();
 
-    // Get and format the pre-reasons
-    const reasons = Object.keys(LOGGING_CATEGORIES).map(reason =>
-        ({ name: formatExpression(reason), value: LOGGING_CATEGORIES[reason] })
-    );
+	// Get and format the pre-reasons
+	const reasons = Object.keys(LOGGING_CATEGORIES).map(reason =>
+		({ name: formatExpression(reason), value: LOGGING_CATEGORIES[reason] })
+	);
 
-    // Get the focussed reason && return the filtered reason
-    const filteredReasons = reasons.filter(reason => reason.name.toLowerCase().includes(focusedReason.toLowerCase()));
-    interaction.respond(filteredReasons);
+	// Get the focussed reason && return the filtered reason
+	const filteredReasons = reasons.filter(reason => reason.name.toLowerCase().includes(focusedReason.toLowerCase()));
+	interaction.respond(filteredReasons);
 }
-
 
 module.exports.run = async (client, interaction) => {
 
@@ -57,19 +56,17 @@ module.exports.run = async (client, interaction) => {
 	const logTypeCategory = Object.keys(logObject)[0];
 	const logTypeCategoryName = Object.values(logObject)[0];
 
-	console.log(`The Interation option is:`, interaction.options.get("log-type").value)
-
 	// Fetch the current guild logchannel for the logTypeCategory
 	const logChannelResponse = await getRequest(`/logchannels/${interaction.guild.id}/${logTypeCategory}`);
 
 	// If there is no log channel for the logTypeCategory, create one
 	if (logChannelResponse.status === 404) {
-		
+
 		const logChannelPostResponse = await postLogChannel(interaction.guild.id, logTypeCategory, targetLogChannel.id)
 
 		// If the request was successful, return success message
 		if (logChannelPostResponse.status === 200) {
-			
+
 			// Send the success embed
 			sendEmbed(interaction, "Set log channel.", `The log channel for \`${logTypeCategoryName}\` has been set to <#${targetLogChannel.id}>.`, SUCCESS);
 
@@ -84,14 +81,14 @@ module.exports.run = async (client, interaction) => {
 
 		console.log(logChannelPostResponse)
 		// If the request was successful, return success message
-		if (logChannelPostResponse.status === 200) {			
+		if (logChannelPostResponse.status === 200) {
 			// Send the success embed
 			sendEmbed(interaction, "Updated log channel.", `The log channel for \`${logTypeCategoryName}\` has been updated to <#${targetLogChannel.id}>. You will now receive logs for \`${logTypeCategoryName}\` in <#${targetLogChannel.id}>.`, WARNING);
 		} else {
 			// If the request was not successful, return an error
 			sendErrorResponse(interaction, `An error occurred while updating the log channel for \`${logTypeCategoryName}\`.`);
 		}
-		
+
 	} else {
 		// If the request was not successful, return an error
 		sendEmbed(interaction, "Error", "An error occurred while setting the log channel.", ERROR);
