0fb3c1c2fd4c5479299c7a0284c48854f8a1790c
Mathijs <Mathijsvanderkuijp@live.nl>
2024-02-20 15:57:40 +0100

    Update Jobs and Levels Seeder

--- packages/api/test/seeders/Levels.seed.js
+++ packages/api/test/seeders/Levels.seed.js
@@ -1,65 +1,25 @@
-const { User, Guild, Levels } = require("../../database/models");
+const { Levels } = require("../../database/models");
 
 module.exports.run = async () => {
 
-    const userData = [
-        {   // FLUXPUCK
-            userId: "270640827787771943",
-            guildId: "660103319557111808",
-            experience: 2458
-        },
-        {   // FLUXPUCK, GUILD 2
-            userId: "270640827787771943",
-            guildId: "253740950315204608",
-        },
-        {   // ZAKARIA, GUILD 1
-            userId: "377842014290575361",
-            guildId: "660103319557111808",
-            experience: 238
-        },
-        {   // ZEUSGMJ, GUILD 1
-            userId: "438054607571386378",
-            guildId: "660103319557111808",
-            experience: 6969
-        },
-        {
-            userId: "1138091030713995344",
-            guildId: "660103319557111808",
-            experience: 700
-        }
-    ]
+    const levelsData = require("../data/levels.json");
 
-    for (const userInfo of userData) {
+    for (const levelInfo of levelsData) {
         try {
-            // Check if the corresponding user exists in the User table
-            const existingUser = await User.findOne({
-                where: {
-                    userId: userInfo.userId,
-                    guildId: userInfo.guildId
-                },
+            // Check if the job already exists
+            const existingLevel = await Levels.findOne({
+                where: { level: levelInfo.level }
             });
 
-            if (existingUser) {
-                // User exists, proceed to create or update the level
-                const existingLevel = await Levels.findOne({
-                    where: {
-                        userId: userInfo.userId,
-                        guildId: userInfo.guildId
-                    },
-                });
-
-                if (existingLevel) {
-                    // Level already exists, update its data
-                    await existingLevel.update(userInfo);
-                } else {
-                    // Level doesn't exist, create a new record
-                    await Levels.create(userInfo);
-                }
+            if (existingLevel) {
+                // Job already exists, update its data
+                await existingLevel.update(levelInfo);
             } else {
-                console.log(`User with does not exist in the User table: ${userInfo.guildId}/${userInfo.userId}`);
+                // Job doesn't exist, create a new record
+                await Levels.create(levelInfo);
             }
         } catch (error) {
-            console.error(`Error creating/updating levels for ${userInfo.guildId}/${userInfo.userId}: ${error.message}`);
+            console.error(`Error creating/updating levels: ${error.message}`);
         }
     }
 
