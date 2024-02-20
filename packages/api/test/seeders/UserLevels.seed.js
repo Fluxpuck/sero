41a7d80342b1240eb0487059d24568d54f131b0f
Mathijs <Mathijsvanderkuijp@live.nl>
2024-02-20 16:01:37 +0100

    Added UserLevels seeder

---
+++ packages/api/test/seeders/UserLevels.seed.js
@@ -0,0 +1,67 @@
+const { User, UserLevels } = require("../../database/models");
+
+module.exports.run = async () => {
+
+    const userData = [
+        {   // FLUXPUCK
+            userId: "270640827787771943",
+            guildId: "660103319557111808",
+            experience: 2458
+        },
+        {   // FLUXPUCK, GUILD 2
+            userId: "270640827787771943",
+            guildId: "253740950315204608",
+        },
+        {   // ZAKARIA, GUILD 1
+            userId: "377842014290575361",
+            guildId: "660103319557111808",
+            experience: 238
+        },
+        {   // ZEUSGMJ, GUILD 1
+            userId: "438054607571386378",
+            guildId: "660103319557111808",
+            experience: 6969
+        },
+        {
+            userId: "1138091030713995344",
+            guildId: "660103319557111808",
+            experience: 700
+        }
+    ]
+
+    for (const userInfo of userData) {
+        try {
+            // Check if the corresponding user exists in the User table
+            const existingUser = await User.findOne({
+                where: {
+                    userId: userInfo.userId,
+                    guildId: userInfo.guildId
+                },
+            });
+
+            if (existingUser) {
+                // User exists, proceed to create or update the level
+                const existingLevel = await UserLevels.findOne({
+                    where: {
+                        userId: userInfo.userId,
+                        guildId: userInfo.guildId
+                    },
+                });
+
+                if (existingLevel) {
+                    // Level already exists, update its data
+                    await existingLevel.update(userInfo);
+                } else {
+                    // Level doesn't exist, create a new record
+                    await UserLevels.create(userInfo);
+                }
+            } else {
+                console.log(`User with does not exist in the User table: ${userInfo.guildId}/${userInfo.userId}`);
+            }
+        } catch (error) {
+            console.error(`Error creating/updating levels for ${userInfo.guildId}/${userInfo.userId}: ${error.message}`);
+        }
+    }
+
+
+}
