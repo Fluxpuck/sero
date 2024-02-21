af0d8ed1a169d60e941aec29c77dec66110ec491
Jackson <jxcksondev@gmail.com>
2024-02-21 11:04:42 -0500

    Add Balance commands

---
+++ packages/bot/commands/economy/balance.js
@@ -0,0 +1,51 @@
+const { getRequest } = require("../../database/connection");
+const {createCustomEmbed} = require("../../assets/embed")
+module.exports.props = {
+    commandName: "balance",
+    description: "Get the balance of a user.",
+    usage: "/reset [user]",
+    interaction: {
+        type: 1,
+        options: [
+            {
+                name: "user",
+                type: 6,
+                description: "Select a user to reset",
+                required: true
+            }
+        ],
+    },
+    defaultMemberPermissions: ['KickMembers'],
+}
+
+module.exports.run = async (client, interaction) => {
+    // Get User details from the interaction options
+    const targetUser = interaction.options.get("user").user;
+
+
+    // Get the user's the experience && check if the user has experience.
+    const result = await getRequest(`/balance/${interaction.guildId}/${targetUser.id}`);
+
+    const { balance } = result.data;
+
+    // Setup Embed:
+    const embed = createCustomEmbed({
+        fields: [
+            {
+                name: `${targetUser.username}'s balance`,
+                value: `:coin: ${balance.toString()} coins`,
+            }
+        ],
+    })
+
+    interaction.reply({
+        embeds: [embed] 
+    })
+}
+
+
+
+
+
+
+
---
+++ packages/bot/commands/economy/remove-money.js
@@ -0,0 +1,53 @@
+const { postRequest, getRequest } = require("../../database/connection");
+
+module.exports.props = {
+    commandName: "remove-money",
+    description: "Remove money from a user.",
+    usage: "/remove-money [user] [amount]",
+    interaction: {
+        type: 1,
+        options: [
+            {
+                name: "user",
+                type: 6,
+                description: "Select a user to remove money from",
+                required: true
+            },
+            {
+                name: "amount",
+                type: 10,
+                description: "The amount of money to remove from the user",
+                required: true,
+                minValue: 1,
+                maxValue: 1000000,
+            },
+        ],
+    },
+    defaultMemberPermissions: ['KickMembers'],
+}
+
+module.exports.run = async (client, interaction) => {
+    // Get User && Amount details from the interaction options
+    const targetUser = interaction.options.get("user").user;
+    const targetAmount = interaction.options.get("amount").value || 0;
+
+
+    // Get the user's balance && check if the user has money.
+    const currentUser = await getRequest(`/balance/${interaction.guildId}/${targetUser.id}`);
+    const currentBalance = currentUser ? currentUser.balance : 0;
+
+    // Check if the user has the proper amount of money.
+    if(currentBalance < targetAmount) {
+       const result = await postRequest(`/balance/${interaction.guildId}/${targetUser.id}`, { amount: -currentBalance })
+       console.log(result.data)
+    } else {
+        // Remove the user's balance if has proper amount.
+       const result = await postRequest(`/balance/${interaction.guildId}/${targetUser.id}`, { amount: -targetAmount });
+       console.log(result.data)
+        return interaction.reply({
+            content: `**${targetAmount.toString()}** coins were removed from <@${targetUser.id}>!`,
+            ephemeral: false
+        })
+    }
+}
+
