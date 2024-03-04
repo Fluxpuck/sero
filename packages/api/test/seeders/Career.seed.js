5e8d0e6b9bab0c0dca9485d9e86c1af5700ff201
Mathijs <Mathijsvanderkuijp@live.nl>
2024-03-04 21:41:43 +0100

    Randomize jobs in career seeder

--- packages/api/test/seeders/Career.seed.js
+++ packages/api/test/seeders/Career.seed.js
@@ -5,12 +5,12 @@ module.exports.run = async () => {
     const userData = [
         {
             userId: "377842014290575361", // ZAKARIAX
-            jobId: 1,
+            jobId: 3,
             guildId: "660103319557111808"
         },
         {
             userId: "438054607571386378", // ZEUSGMJ
-            jobId: 1,
+            jobId: 7,
             guildId: "660103319557111808"
         },
         {
@@ -20,7 +20,7 @@ module.exports.run = async () => {
         },
         {
             userId: "1138091030713995344",
-            jobId: 1,
+            jobId: 9,
             guildId: "660103319557111808",
         }
     ]
@@ -28,18 +28,18 @@ module.exports.run = async () => {
     for (const userInfo of userData) {
         try {
             // Check if the guild with the specified guildId exists
-                    // User exists get job
-                    const existingJob = await UserCareers.findOne({
-                        where: {
-                            userId: userInfo.userId,
-                            guildId: userInfo.guildId,
-                        }
-                    })
-                    if(existingJob) {
-                        existingJob.update(userInfo)
-                    } else {
-                        UserCareers.create(userInfo)
-                    }
+            // User exists get job
+            const existingJob = await UserCareers.findOne({
+                where: {
+                    userId: userInfo.userId,
+                    guildId: userInfo.guildId,
+                }
+            })
+            if (existingJob) {
+                existingJob.update(userInfo)
+            } else {
+                UserCareers.create(userInfo)
+            }
         } catch (error) {
             console.error(`Error creating/updating job: ${error.message}`);
         }
