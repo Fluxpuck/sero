d2d37438d0a80989c740b1d0ab545a9445e28d5a
Mathijs <Mathijsvanderkuijp@live.nl>
2024-03-05 23:28:01 +0100

    Add transfer-money command

---
+++ packages/bot/commands/economy/transfer-money.js
@@ -0,0 +1,65 @@
+const { postRequest } = require("../../database/connection");
+
+module.exports.props = {
+    commandName: "transfer-money",
+    description: "Transfer money to another user",
+    usage: "/transfer-money [user] [amount]",
+    interaction: {
+        type: 1,
+        options: [
+            {
+                name: "user",
+                type: 6,
+                description: "Select a user to transfer money to",
+                required: true
+            },
+            {
+                name: "amount",
+                type: 10,
+                description: "The amount of coins to transfer to the user",
+                required: true,
+                minValue: 1,
+                maxValue: 10_000,
+            },
+        ],
+    }
+}
+
+module.exports.run = async (client, interaction) => {
+    // Get User && Amount details from the interaction options
+    const targetUser = interaction.options.get("user").user;
+    const transferAmount = interaction.options.get("amount").value;
+
+    /**
+     * This code whas a 24 hour cooldown per user
+     */
+    const cooldownKey = targetUser.id + interaction.id
+    if (client.cooldowns.has(cooldownKey) === false) {
+
+        // Remove balance from the author
+        const removeResult = await postRequest(`/balance/${interaction.guildId}/${interaction.user.id}`, { amount: -targetAmount })
+        // Add balance to the target
+        const addResult = await postRequest(`/balance/${interaction.guildId}/${targetUser.id}`, { amount: +targetAmount });
+
+        // If either request was not successful, return an error
+        if (removeResult.status !== 200 || addResult.status !== 200) {
+            interaction.reply({
+                content: "Something went wrong while transferring money to the user.",
+                ephemeral: true
+            })
+        } else {
+            interaction.reply({
+                content: `<@${targetUser.id}> has recieved **${transferAmount}** of your money!`,
+                ephemeral: false
+            })
+        }
+
+        // Add the user to the cooldowns Collection
+        return client.cooldowns.set(cooldownKey, interaction, 24 * 60 * 60) // 3600 minutes 
+    } else {
+        return interaction.reply({
+            content: `You can only transfer experience once per day!`,
+            ephemeral: true
+        })
+    }
+}
