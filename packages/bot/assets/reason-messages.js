62e771475ed4a7771d3c764060a5e58f0c6aedfc
Mathijs <Mathijsvanderkuijp@live.nl>
2024-03-10 22:07:32 +0100

    Update pre-defined moderation action reasons

--- packages/bot/assets/reason-messages.js
+++ packages/bot/assets/reason-messages.js
@@ -1,34 +1,47 @@
 const KICK_PREREASONS = {
-    IMPERSONATION: "impersonating another staff member or celebrity.",
-    INAPPROPRIATE_USENRNAME: "innapropriate username.",
-    ALT_ACCOUNT: "alt account."
+    IMPERSONATION: "Impersonating a staff member or another user.",
+    INAPPROPRIATE_USERNAME: "Having an innapropriate username.",
+    ALT_ACCOUNT: "Using an alt account.",
+    MULTIPLE_VIOLATIONS: "Multiple server rule infringements.",
+    RACIAL_SLURS: "Saying racial slurs in chat.",
+    DISCORD_TOS_VIOLATION: "Violating Discord Terms of Service.",
+    NSFW_VIOLATION: "Posting NSFW content.",
+    UNDERAGE_USER: "Being under the age of 12, violating Discord ToS."
 }
+
 const BAN_PREREASONS = {
-    MULTIPLE_VIOLATIONS: "multiple server rule infringements.",
-    RACIAL_SLURS: "saying racial slurs in a chat.",
-    DISCORD_TOS_VIOLATION: "was banned for violating Discord Terms of Service.",
-    NSFW_VIOLATION: "was banned for NSFW violations.",
-    UNDERAGE_USER: "was banned for being under the age of 12 violating Discord Terms of Service."
+    IMPERSONATION: "Impersonating a staff member or another user.",
+    INAPPROPRIATE_USERNAME: "Having an innapropriate username.",
+    ALT_ACCOUNT: "Using an alt account.",
+    MULTIPLE_VIOLATIONS: "Multiple server rule infringements.",
+    RACIAL_SLURS: "Saying racial slurs in chat.",
+    DISCORD_TOS_VIOLATION: "Violating Discord Terms of Service.",
+    NSFW_VIOLATION: "Posting NSFW content.",
+    UNDERAGE_USER: "Being under the age of 12, violating Discord ToS."
 }
 
 const WARN_PREREASONS = {
-    IMPERSONATION: "Impersonation of a staff member of another user.",
-    INAPPROPRIATE_USERNAME: "Innapropriate username.",
-    ALT_ACCOUNT: "Alt account.",
-    MULTIPLE_VIOLATIONS: "Multiple server rule infringements.",
+    IMPERSONATION: "Impersonation of a staff member or another user.",
+    INAPPROPRIATE_USERNAME: "You have an innapropriate username.",
+    ALT_ACCOUNT: "You account has been flagged as an alt account.",
+    MULTIPLE_VIOLATIONS: "This is a warning for multiple server rule infringements.",
     RACIAL_SLURS: "Using racial slurs in chat.",
     DISCORD_TOS_VIOLATION: "Violating Discord Terms of Service",
     NSFW_VIOLATION: "Posting NSFW content.",
     UNDERAGE_USER: "Being under the age of 12, violating Discord ToS."
 }
-const MUTE_PREREASONS = {
-    SPAMMING:  "spamming excessively in a chat.",
-    CURSING: "cursing excessively in a chat.",
-    BULLYING: "bullying another member.",
-    FOREIGN_LANGUAGE: "speaking in a foreign language.",
-    MINI_MODDING: "mini-modding.",
-    CREATING_DRAMA: "creating drama in the chat."
-}
 
+const MUTE_PREREASONS = {
+    SPAMMING: "Spamming excessively in a chat.",
+    CURSING: "Cursing excessively in a chat.",
+    BULLYING: "Bullying another member.",
+    FOREIGN_LANGUAGE: "Speaking in a foreign language.",
+    MINI_MODDING: "Mini-modding.",
+    CREATING_DRAMA: "Creating drama in the chat.",
+    SPECIAL_CHARACTERS: "Using special characters excessively in a chat.",
+    CAPS_LOCK: "Typing in all caps excessively in a chat.",
+    ADVERTISING: "Advertising a server or website.",
+    INAPPROPRIATE_CONTENT: "Talking about inappropriate things in a chat.",
+}
 
 module.exports = { KICK_PREREASONS, BAN_PREREASONS, WARN_PREREASONS, MUTE_PREREASONS }
