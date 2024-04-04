4085e802f293db70c7583f9ebc6209eb195e6962
Mathijs <Mathijsvanderkuijp@live.nl>
2024-04-04 02:13:22 +0200

    TO-DO list

--- packages/bot/events/guildMemberRank.js
+++ packages/bot/events/guildMemberRank.js
@@ -10,12 +10,22 @@ module.exports = async (client, payload) => {
 
     console.log("guildMemberRank.js: ", payload);
 
-    /**
+    /** LEVEL ROUTE
+     * @one - Add a function that will check the ranks of the guild
+     * @two - If the member is higher than the level of a rank, 
+     * @three Add this rank to the member && @four Send a RabbitMQ message with
+     * @five - (the guildId, userId, level and (highest)rank)
+     */
+
+    /** THIS EVENT
      * @one - Get the member_levels and rank-roles from the API
      * @two - Filter all rank-roles with lower level than the member's level
      * @three - Find the member and the roles in the guild
      * @four - Give the member the rank-roles
      */
+
+
+
 
 
 
