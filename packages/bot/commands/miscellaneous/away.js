2d3ed62a66e0189b34b1a81e0b5c0e38362f3725
Mathijs <33183946+Fluxpuck@users.noreply.github.com>
2024-05-04 20:09:24 +0200

    SERO-64 - Add Message option to Away command (#68)
    * First commit, init new field database
    
    * Finished issue, added message field to database, model and command of AWAY. Added prereasons for the away message
    
    * smoll updates
    
    ---------
    
    Co-authored-by: Zakaria <62657990+ZakariaX1@users.noreply.github.com>

--- packages/bot/commands/miscellaneous/away.js
+++ packages/bot/commands/miscellaneous/away.js
@@ -1,32 +1,76 @@
+const { AWAY_PREREASONS } = require("../../assets/reason-messages");
 const { postRequest } = require("../../database/connection");
+const { formatExpression } = require("../../lib/helpers/StringHelpers/stringHelper");
 
 module.exports.props = {
     commandName: "away",
     description: "Let everyone know you're away",
-    usage: "/away [time]",
+    usage: "/away (time) (message)",
     interaction: {
         type: 1,
         options: [
             {
                 name: "time",
                 type: 10,
-                description: "The amount (in minutes) of time you want to be away for",
+                description: "The amount (in minutes) of time you want to be away for (default is 5 minutes)",
                 required: false,
                 minValue: 5,
                 maxValue: 720,
+                autocomplete: true
+            },
+            {
+                name: "message",
+                description: "The reason you are away",
+                type: 3,
+                required: false,
+                autocomplete: true,
+                maxLength: 250,
             },
         ],
     },
     defaultMemberPermissions: ['SendMessages'],
 }
 
+module.exports.autocomplete = async (client, interaction) => {
+    const focusedReason = interaction.options.getFocused(true);
+
+    if (focusedReason.name === "time") {
+        defaultTimes = [
+            { name: "10 minutes", value: "10" },
+            { name: "30 minutes", value: "30" },
+            { name: "1 hour", value: "60" },
+            { name: "3 hours", value: "180" },
+            { name: "6 hours", value: "360" },
+            { name: "12 hours", value: "720" },
+        ];
+        if (focusedReason.value) {
+            const filteredTimes = defaultTimes.filter(time => time.name.toLowerCase().includes(focusedReason.value.toLowerCase()));
+            await interaction.respond(filteredTimes);
+            return;
+        }
+        await interaction.respond(defaultTimes);
+        return;
+    }
+
+    if (focusedReason.name === "message") {
+        const reasons = Object.keys(AWAY_PREREASONS).map(reason =>
+            ({ name: formatExpression(reason), value: AWAY_PREREASONS[reason] })
+        );
+
+        const filteredReasons = reasons.filter(reason => reason.name.toLowerCase().includes(focusedReason.value.toLowerCase()));
+        await interaction.respond(filteredReasons);
+        return;
+    }
+}
+
 module.exports.run = async (client, interaction) => {
     // Get Away time value from the interaction options
     const timeOption = interaction.options.get("time")?.value;
     const timeInMinutes = timeOption ?? 5; // Default to 5 minutes
+    const messageOption = interaction.options.get("message")?.value;
 
     // Give the user the experience
-    const result = await postRequest(`/away/${interaction.guildId}/${interaction.user.id}`, { duration: timeInMinutes });
+    const result = await postRequest(`/away/${interaction.guildId}/${interaction.user.id}`, { duration: timeInMinutes, message: messageOption });
 
     // If the request was not successful, return an error
     if (result.status !== 200) {
@@ -35,8 +79,13 @@ module.exports.run = async (client, interaction) => {
             ephemeral: true
         })
     } else {
+        let content = `<@${interaction.user.id}> will be away for **${timeInMinutes}** minutes!`;
+        if (interaction.options.get("message")) {
+            const reason = interaction.options.get("message")?.value;
+            content += `> **${reason}**`;
+        }
         return interaction.reply({
-            content: `<@${interaction.user.id}> will be away for **${timeInMinutes}** minutes!`,
+            content: content,
             ephemeral: false
         }).then(msg => {
             setTimeout(() => {
