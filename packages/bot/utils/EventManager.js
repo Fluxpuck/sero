76878a786774fbc860fed557cc931d6817fd4b2e
Mathijs <Mathijsvanderkuijp@live.nl>
2024-05-27 18:57:41 +0200

    Make the eventManager function recursive

--- packages/bot/utils/EventManager.js
+++ packages/bot/utils/EventManager.js
@@ -1,18 +1,37 @@
-const { readdirSync } = require('fs');
-const { join } = require('path');
+const { readdirSync, statSync } = require('fs');
+const { join, basename, extname } = require('path');
 
 module.exports.run = (client) => {
 
-    //set directory path to events and read files
+    // Define the path to the events directory
     const filePath = join(__dirname, '..', 'events');
-    const eventFiles = readdirSync(filePath);
+
+    // Function to get all files in a directory
+    function getAllFiles(dirPath, arrayOfFiles) {
+        // Read all files in the directory
+        const files = readdirSync(dirPath);
+        arrayOfFiles = arrayOfFiles || [];
+        // Loop through each file in the directory
+        files.forEach((file) => {
+            if (statSync(join(dirPath, file)).isDirectory()) {
+                arrayOfFiles = getAllFiles(join(dirPath, file), arrayOfFiles);
+            } else {
+                arrayOfFiles.push(join(dirPath, file));
+            }
+        });
+
+        return arrayOfFiles;
+    }
+
+    // Get all files in the events directory
+    const eventFiles = getAllFiles(filePath);
 
     // Loop through each file in the events directory
     for (const file of eventFiles) {
         // Load the event from the file
-        const event = require(`${filePath}/${file}`);
+        const event = require(file);
         // Get the name of the event from the file name
-        const eventName = file.split('.').shift();
+        const eventName = basename(file, extname(file));
 
         // Bind the event to the client
         client.on(eventName, event.bind(null, client));
