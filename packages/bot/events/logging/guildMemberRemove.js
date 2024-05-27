76c8b59f8001ccdd9fc9fe9036b66c70c52aecc6
Mathijs <Mathijsvanderkuijp@live.nl>
2024-05-27 18:57:50 +0200

    Move and add events to /logging

--- packages/bot/events/logging/guildMemberRemove.js
+++ packages/bot/events/logging/guildMemberRemove.js
@@ -1,6 +1,17 @@
-const { deleteRequest } = require("../database/connection");
+const { deleteRequest } = require("../../database/connection");
 
 module.exports = async (client, member) => {
+
+    // Deactivate user in database
     await deleteRequest(`/users/deactivate/${member.guild.id}/${member.id}}`);
+
+
+
+
+
+
+
+
+
     return;
 }
