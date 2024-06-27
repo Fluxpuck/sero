97be721c5ab0baac9fe0fd8a1089fddc70eb60ba
Mathijs <Mathijsvanderkuijp@live.nl>
2024-06-27 19:18:01 +0200

    refactor: improved code for base /users route

--- packages/api/routes/users/index.js
+++ packages/api/routes/users/index.js
@@ -4,161 +4,142 @@ const { User } = require("../../database/models");
 const { sequelize } = require('../../database/sequelize');
 const { CreateError } = require('../../utils/ClassManager');
 
-/**
- * @router GET api/users/:guildId
- * @description Get all Users
- */
-router.get("/", async (req, res, next) => {
-    try {
-        const { guildId } = req.params;
-        const limit = req.query.limit || 200000;
+// ==================================================
 
-        // Find all users
-        const result = await User.findAll({
+const DEFAULT_LIMIT = 200_000; // Default limit for the query
+
+/**     
+ * Route to fetch all members from a param guild
+ * @router GET api/users/:guildId 
+ */
+router.get("/:guildId", async (req, res, next) => {
+    try {
+
+        // Get the limit from the request query
+        const limit = Math.round(parseInt(req.query.limit, 10)) || DEFAULT_LIMIT;
+
+        // Get the guildId from the request params
+        const { guildId } = req.params;
+
+        // Find all Users related to the guildId
+        await User.findAll({
             where: { guildId: guildId },
             limit: limit,
+        }).then((result) => {
+
+            // If no results found, trigger error
+            if (!result) throw new CreateError(404, 'No users were found');
+
+            // Return the results
+            return res.status(200).json(result);
+
         });
 
-        // If no results found, trigger error
-        if (!result) {
-            throw new CreateError(404, 'No users were found');
-        }
-
-        // Return the results
-        return res.status(200).json(result);
-
     } catch (error) {
-        next(error);
+        next(error); // Pass the error to the next middleware
     }
 });
 
 /**
+ * Route to Fetch a specific User from a param Guild
  * @router GET api/users/:guildId/:userId
- * @description Get a specific User
  */
 router.get("/:guildId/:userId", async (req, res, next) => {
     try {
+
+        // Get the guildId and userId from the request params
         const { guildId, userId } = req.params;
 
-        // Check for results related to the guildId
-        const result = await User.findOne({
-            where: {
-                guildId: guildId,
-                userId: userId
-            },
+        // Find the User related to the guildId and userId
+        await User.findOne({
+            where: { guildId: guildId, userId: userId }
+        }).then((result) => {
+
+            // If no results found, trigger error
+            if (!result) throw new CreateError(404, 'User was not found');
+
+            // Return the results
+            return res.status(200).json(result);
+
         });
 
-        // If no results found, trigger error
-        if (!result) {
-            throw new CreateError(404, 'User was not found');
-        }
-
-        // Return the results
-        res.status(200).json(result);
-
     } catch (error) {
-        next(error);
+        next(error); // Pass the error to the next middleware
     }
 });
 
-
-// Setup Attributes for this Route
-const requiredProperties = ['userName'];
+// ==================================================
 
 /**
+ * Route to create or update a user
  * @router POST api/users/:guildId/:userId
- * @description Create or update a User
  */
 router.post('/:guildId/:userId', async (req, res, next) => {
-    const t = await sequelize.transaction();
+    const t = await sequelize.transaction(); // Start a transaction
     try {
-        const { body, params } = req;
-        const { guildId, userId } = params;
+        // Get the guildId and userId from the request params
+        const { guildId, userId } = req.params;
 
-        // Check if the request body has all required properties
-        if (!body || Object.keys(body).length === 0 || requiredProperties.some(prop => body[prop] === undefined)) {
+        // Get the data from request body
+        const { userName } = req.body;
+
+        // Check if the request body contains trhe required properties
+        if (!userName) {
             throw new CreateError(400, 'Invalid or missing data for this request');
         }
 
-        // Get the data from request body && create object
-        const { userName } = body;
-        const updateData = {
-            guildId: guildId,
-            userId: userId,
-            userName: userName
-        }
+        // Prepare the data to be updated
+        const requestData = { guildId, userId, userName };
 
-        // Check if the guild already exists
-        const request = await User.findOne({
-            where: {
-                guildId: guildId,
-                userId: userId
-            },
-            transaction: t,
-        });
+        // Use upsert to either update or create the user
+        const [user, created] = await User.upsert(requestData, { transaction: t });
 
-        // Update or Create the request
-        if (request) {
-            const result = await request.update(updateData, { transaction: t });
-            res.status(200).json({
-                message: `User ${userId} was updated successfully`,
-                data: result
-            });
-        } else {
-            const result = await User.create(updateData, { transaction: t });
-            res.status(200).json({
-                message: `User ${userId} was created successfully`,
-                data: result
-            });
-        }
+        // Commit the transaction
+        await t.commit();
 
-        // Commit and finish the transaction
-        return t.commit();
+        // Return the results
+        const message = created
+            ? `User ${userId} was created successfully for ${guildId}`
+            : `User ${userId} was updated successfully for ${guildId}`;
+        res.status(200).json({ message, data: user });
 
     } catch (error) {
-        await t.rollback();
-        next(error);
+        await t.rollback(); // Rollback the transaction
+        next(error); // Pass the error to the next middleware
     }
 });
+
+// ==================================================
 
 /**
+ * Route to delete a user from a param guild
  * @router DELETE api/users/:guildId/:userId
- * @description Delete a User
  */
-router.delete("/:guildId/:userId", async (req, res, next) => {
-    const t = await sequelize.transaction();
+router.delete('/:guildId/:userId', async (req, res, next) => {
     try {
+
+        // Get the guildId and userId from the request params
         const { guildId, userId } = req.params;
 
-        // Check if the guild already exists
-        const request = await User.findOne({
-            where: {
-                guildId: guildId,
-                userId: userId
-            },
-            transaction: t,
+        await User.destroy({
+            where: { guildId, userId },
+            transaction: t
+        }).then((result) => {
+
+            // If no results found, trigger error
+            if (!result) throw new CreateError(404, 'User was not found');
+
+            // Return the results
+            return res.status(200).json({
+                message: `User ${userId} was deleted successfully`,
+                data: result
+            });
+
         });
-
-        // If no results found, trigger error
-        if (!request) {
-            throw new CreateError(404, 'User was not found');
-        }
-
-        // Delete the request
-        await request.destroy({ transaction: t });
-        res.status(200).json({
-            message: `User ${userId} was deleted successfully`,
-            data: request
-        });
-
-        // Commit and finish the transaction
-        return t.commit();
 
     } catch (error) {
-        await t.rollback();
-        next(error);
+        next(error); // Pass the error to the next middleware
     }
 });
 
-// → Export Router to App
-module.exports = router;
+module.exports = router; // Export the router
