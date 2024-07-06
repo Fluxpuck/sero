8b151e00e86b73b7d67af3af59e34917834212de
Mathijs <Mathijsvanderkuijp@live.nl>
2024-07-06 11:58:11 +0200

    refactor: Add baseRoute and update routePath

--- packages/api/middleware/routes.js
+++ packages/api/middleware/routes.js
@@ -1,21 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 
-function loadRoutes(app, dirPath) {
+function loadRoutes(app, dirPath, baseRoute = '') {
     fs.readdirSync(dirPath).forEach(file => {
         const filePath = path.join(dirPath, file);
         const stat = fs.statSync(filePath);
 
         if (stat.isDirectory()) {
             // Recursively load routes in subdirectories
-            loadRoutes(app, filePath);
+            const newBaseRoute = `${baseRoute}/${file}`;
+            loadRoutes(app, filePath, newBaseRoute);
         } else if (file.endsWith('.js')) {
             const route = require(filePath);
-            const routePath = filePath
-                .replace(__dirname, '')
-                .replace(/\\/g, '/')
-                .replace('/routes', '')
-                .replace('.js', '');
+            const routePath = `${baseRoute}/${file.replace('.js', '')}`;
 
             console.log(`Loading route: ${routePath}`);
 
