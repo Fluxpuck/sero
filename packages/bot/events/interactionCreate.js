cadd5e77f1b02b8fefde3b1b27197a3594a6bf46
Mathijs <Mathijsvanderkuijp@live.nl>
2024-09-07 20:28:36 +0200

    feat: Ignore command cooldowns on development

--- packages/bot/events/interactionCreate.js
+++ packages/bot/events/interactionCreate.js
@@ -16,22 +16,24 @@ module.exports = async (client, interaction) => {
 
         // Check if the command has a cooldown
         // Check if the user is on a cooldown or add them to a cooldown
-        if (commandFile.props.cooldown) {
-            const cooldown_key = `${interaction.user.id}_${interaction.guildId}_${kebabCase(interaction.commandName)}`;
-            const cooldown_timer = commandFile.props.cooldown || 0;
+        if (process.env.NODE_ENV != "development") {
+            if (commandFile.props.cooldown) {
+                const cooldown_key = `${interaction.user.id}_${interaction.guildId}_${kebabCase(interaction.commandName)}`;
+                const cooldown_timer = commandFile.props.cooldown || 0;
 
-            if (client.cooldowns.has(cooldown_key)) {
-                const ttl = client.cooldowns.getTtl(cooldown_key);
-                const remainingTime = Math.ceil((ttl - Date.now()) / 1000); // Convert milliseconds to seconds
+                if (client.cooldowns.has(cooldown_key)) {
+                    const ttl = client.cooldowns.getTtl(cooldown_key);
+                    const remainingTime = Math.ceil((ttl - Date.now()) / 1000); // Convert milliseconds to seconds
 
-                return interaction.reply({
-                    content: `This command is on a cooldown! Please wait ${remainingTime} more seconds.`,
-                    ephemeral: true
-                });
+                    return interaction.reply({
+                        content: `This command is on a cooldown! Please wait ${remainingTime} more seconds.`,
+                        ephemeral: true
+                    });
+                }
+
+                // Add the user to a cooldown
+                client.cooldowns.set(cooldown_key, interaction, cooldown_timer);
             }
-
-            // Add the user to a cooldown
-            client.cooldowns.set(cooldown_key, interaction, cooldown_timer);
         }
 
         // Check if the interaction has an autocomplete function
