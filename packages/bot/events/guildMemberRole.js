3a2cbc96a344bad2f5f8a64fb0816393a14b49a8
Mathijs <Mathijsvanderkuijp@live.nl>
2024-09-10 20:26:10 +0200

    feat: guildMemberRole
    to remove roles based on the data provided in the payload

---
+++ packages/bot/events/guildMemberRole.js
@@ -0,0 +1,26 @@
+const { findUser } = require("../lib/resolvers/userResolver");
+
+module.exports = async (client, payload = []) => {
+
+    // Check if all required attributes exist in the payload
+    const requiredAttributes = ['guildId', 'userId', 'roleId'];
+    for (const attribute of requiredAttributes) {
+        if (!payload.hasOwnProperty(attribute)) return;
+    }
+
+    // Get the guild by guildId and the member by userId
+    const guild = await client.guilds.fetch(payload.guildId);
+    const member = findUser(guild, payload.userId);
+    const role = await guild.roles.fetch(payload.roleId);
+
+    try {
+        // Remove the role from the member
+        await member?.roles?.remove(role, `Remove temp role ${role.name}`).catch(err => {
+            throw new Error(`Error removing temp role ${role.name} from ${member.name}`, err);
+        });
+
+    } catch (err) {
+        console.error(err);
+    };
+
+};
