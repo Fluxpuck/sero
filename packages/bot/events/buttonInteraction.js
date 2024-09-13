6ae2d13987a000c228733a3575289a95469e197c
Mathijs <Mathijsvanderkuijp@live.nl>
2024-09-13 04:39:05 +0200

    feat: add claim-exp-reward to buttonInteraction

--- packages/bot/events/buttonInteraction.js
+++ packages/bot/events/buttonInteraction.js
@@ -1,21 +1,42 @@
-const ButtonInteractions = {
-    "button": "BUTTON_CLICK",
-    /**
-     * @OPTIONAL - It's possible to execute a command (without params)...
-     * "ping": require("../commands/miscellaneous/ping")
-     */
-}
+const { postRequest } = require("../database/connection");
 
 module.exports = async (client, interaction) => {
+
+
+
 
     /**
      * Check for button interactions that have to do with /get
      * @ButtonInteractions {Object} 
      */
-    switch (ButtonInteractions[interaction.customId]) {
-        case "BUTTON_CLICK":
+    switch (interaction.customId) {
+        case "claim-exp-reward":
 
-            // PERFORM ACTIONS HERE
+            // Calculate a random targetAmount between 100 and 1000
+            const min = 100, max = 1_000;
+            const targetAmount = Math.floor(Math.random() * (max - min + 1)) + min;
+
+            try {
+                // Give the user the experience
+                const result = await postRequest(`/guilds/${interaction.guildId}/levels/exp/${interaction.member.id}`, { experience: targetAmount });
+
+                // If the request was successful
+                // Return message to the user
+                if (result?.status === 200) {
+
+                    // Delete the message that the button was associated with
+                    await interaction.deferUpdate();
+                    await interaction.message.delete();
+
+                    // Return the message to the user
+                    return interaction.followUp({
+                        content: `Congratulations! You have claimed **${targetAmount}** experience!`,
+                        ephemeral: true
+                    })
+                }
+            } catch (error) {
+                console.error
+            }
 
             break;
     }
