797a7580379b6dec2456951b31ed1c759422fc5b
Mathijs <Mathijsvanderkuijp@live.nl>
2024-09-15 21:27:51 +0200

    chore: cleanup

--- packages/bot/events/logging/guildMemberRemove.js
+++ packages/bot/events/logging/guildMemberRemove.js
@@ -1,15 +1,4 @@
-const { deleteRequest } = require("../../database/connection");
-
 module.exports = async (client, member) => {
-
-    // Deactivate user in database
-    await deleteRequest(`/users/deactivate/${member.guild.id}/${member.id}}`);
-
-
-
-
-
-
 
 
 
