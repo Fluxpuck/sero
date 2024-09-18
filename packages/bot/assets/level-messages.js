276a630ea9aa7c51ebfe1123d0095c3998229290
Mathijs <Mathijsvanderkuijp@live.nl>
2024-09-18 16:27:56 +0200

    refactor: Update Level Up Messages

--- packages/bot/assets/level-messages.js
+++ packages/bot/assets/level-messages.js
@@ -1,10 +1,14 @@
 const LEVEL_MESSAGES = [
-    "Congratulations {AUTHOR}! You have leveled up to level {LEVEL} 🎉",
-    "LEVEL UP! {AUTHOR} is now level {LEVEL}!",
-    "{AUTHOR} is now level {LEVEL} 👍",
-    "{AUTHOR} has reached level {LEVEL}!",
-    "{AUTHOR} has reached level {LEVEL}! Congratulations! 🎉",
-    "Congratulations {AUTHOR}! You have reached level {LEVEL}!",
+    "Congratulations! {AUTHOR} has just unlocked a new level of awesomeness at **level {LEVEL}**! 🎉",
+    "Level up! {AUTHOR}'s skills have reached legendary status at **level {LEVEL}**! 🌟",
+    "Boom! {AUTHOR} has leveled up to **level {LEVEL}** and left the competition in the dust! 💥",
+    "New level unlocked! {AUTHOR}'s journey to greatness continues at **level {LEVEL}**! 🚀",
+    "{AUTHOR} has leveled up to **level {LEVEL}**! Time to show off those new powers! 💪",
+    "Ding! {AUTHOR} has reached **level {LEVEL}**. Keep up the amazing work! 🔔",
+    "Level up achieved! {AUTHOR}'s dedication is paying off at **level {LEVEL}**! 🏆",
+    "{AUTHOR} has just leveled up to **level {LEVEL}**! The adventure gets even more exciting from here! 🗺️",
+    "Congrats! {AUTHOR} has leveled up to **level {LEVEL}** and earned a badge of honor! 🥇",
+    "{AUTHOR} has reached **level {LEVEL}**! The sky's the limit! 🌌"
 ];
 
 module.exports = { LEVEL_MESSAGES }
