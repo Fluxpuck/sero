387a2c4950eda79cb71478a4a9f7bb5a5f940d0c
Mathijs <Mathijsvanderkuijp@live.nl>
2024-09-18 17:03:41 +0200

    refactor: level messages

--- packages/bot/assets/level-messages.js
+++ packages/bot/assets/level-messages.js
@@ -1,13 +1,13 @@
 const LEVEL_MESSAGES = [
     "Congratulations! {AUTHOR} has just unlocked a new level of awesomeness at **level {LEVEL}**! 🎉",
-    "Level up! {AUTHOR}'s skills have reached legendary status at **level {LEVEL}**! 🌟",
-    "Boom! {AUTHOR} has leveled up to **level {LEVEL}** and left the competition in the dust! 💥",
+    "Level up! {AUTHOR}'s typing-skills is reaching legendary status at **level {LEVEL}**! 🌟",
+    "Boom! {AUTHOR} has leveled up to **level {LEVEL}** and is leaving the competition in the dust! 💥",
     "New level unlocked! {AUTHOR}'s journey to greatness continues at **level {LEVEL}**! 🚀",
-    "{AUTHOR} has leveled up to **level {LEVEL}**! Time to show off those new powers! 💪",
+    "Power move! {AUTHOR} has leveled up to **level {LEVEL}**! 💪",
     "Ding! {AUTHOR} has reached **level {LEVEL}**. Keep up the amazing work! 🔔",
     "Level up achieved! {AUTHOR}'s dedication is paying off at **level {LEVEL}**! 🏆",
-    "{AUTHOR} has just leveled up to **level {LEVEL}**! The adventure gets even more exciting from here! 🗺️",
-    "Congrats! {AUTHOR} has leveled up to **level {LEVEL}** and earned a badge of honor! 🥇",
+    "{AUTHOR} has just leveled up to **level {LEVEL}**! The adventure only gets more exciting from here! 🗺️",
+    "{AUTHOR} has reached **level {LEVEL}** and earned an honorable mention 🥇",
     "{AUTHOR} has reached **level {LEVEL}**! The sky's the limit! 🌌"
 ];
 
