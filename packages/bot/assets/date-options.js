7556084d3e69197b51c2565857e9ba4eeb7c7b15
Zakaria <62657990+ZakariaX1@users.noreply.github.com>
2024-09-19 12:23:14 +0200

    Feat: Added date options to assets for hardcoded months and month day options

---
+++ packages/bot/assets/date-options.js
@@ -0,0 +1,37 @@
+// Description: This file contains the options for the date picker in the bot.
+const monthOptions = {
+    1: 'January',
+    2: 'February',
+    3: 'March',
+    4: 'April',
+    5: 'May',
+    6: 'June',
+    7: 'July',
+    8: 'August',
+    9: 'September',
+    10: 'October',
+    11: 'November',
+    12: 'December',
+};
+
+// Possible days in a month (key is the month #, value is the number of days)
+const dayPossibilities = {
+    1: 31,
+    2: 29,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31,
+};
+
+module.exports = {
+    monthOptions,
+    dayPossibilities
+}
+    
