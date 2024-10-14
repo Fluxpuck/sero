ba33b185c201736050b7b7293c4e8d82d5536d22
Mathijs <Mathijsvanderkuijp@live.nl>
2024-10-15 01:34:13 +0200

    refactor: tweak reward-drop eligibles

--- packages/bot/events/guildRewardDrops.js
+++ packages/bot/events/guildRewardDrops.js
@@ -20,7 +20,7 @@ module.exports = async (client, payload) => {
         const channel = await guild.channels.fetch(payload.channelId);
 
         // Fetch the last 100 messages in the channel
-        const eligibleIds = await getUniqueAuthorsFromMessages(channel);
+        const eligibleIds = await getUniqueAuthorsFromMessages(channel, 10);
         if (eligibleIds.length === 0) return;
 
         // Set the rewardDrop object in the guild
