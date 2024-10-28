4763665853c2a4f655e79ad7b039c83cb14de3a6
Mathijs <Mathijsvanderkuijp@live.nl>
2024-10-29 00:26:25 +0100

    refactor: reward messages and gifs

--- packages/bot/assets/reward-messages.js
+++ packages/bot/assets/reward-messages.js
@@ -1,17 +1,24 @@
 const REWARD_MESSAGES = [
-    "Woohoo! You've found a random reward drop!",
-    "A wild reward drop has been spotted!",
-    "You've found a reward drop!",
+    "A wild reward drop has appeared!",
+    "Woohoo! You've found a reward drop!",
     "A reward drop has been discovered!",
-    "Woah! A reward drop has been found!",
     "Sero dropped a reward for you!",
+    "Woah! Is that a reward drop?",
+    "It's raining rewards!",
+    "A reward drop has landed!",
 ];
 
 const REWARD_GIFS = [
     "https://media1.tenor.com/m/V0Z6XInA21UAAAAC/teletubbies-dipsy.gif",
     "https://media1.tenor.com/m/wer_TRGa6kQAAAAC/gifts-spongebob.gif",
     "https://media1.tenor.com/m/6zFWZXk3RmAAAAAC/surprise-birthday.gif",
-    "https://media.tenor.com/Lkjf2fr8C90AAAAi/cat-kitty.gif"
+    "https://media.tenor.com/tD4y6qotBCMAAAAi/fat-cat-gift-for-you.gif",
+    "https://media1.tenor.com/m/j_GMiruOTOwAAAAC/spongebob-unicorn.gif",
+    "https://media1.tenor.com/m/Z7v5hwsD2VAAAAAd/what%27s-in-the-box-buttercup.gif",
+    "https://media1.tenor.com/m/dm1dZGrVryAAAAAC/bling-diva.gif",
+    "https://media1.tenor.com/m/tfHUTmqf4foAAAAC/nifty-league-nifty-smashers.gif",
+    "https://media1.tenor.com/m/LMHWuWfylXgAAAAd/mario-super-mario.gif",
+    "https://media1.tenor.com/m/paesy669JUIAAAAC/gift-present.gif",
 ];
 
 module.exports = { REWARD_MESSAGES, REWARD_GIFS }
