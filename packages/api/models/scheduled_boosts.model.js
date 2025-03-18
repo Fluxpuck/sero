04324276c1f42e9c0d986b945510ddd5d6294e7d
ZakariaX <62657990+ZakariaX1@users.noreply.github.com>
2025-03-18 13:16:02 +0100

    refactor: clean up scheduled boosts model by removing unused code and comments

--- packages/api/models/scheduled_boosts.model.js
+++ packages/api/models/scheduled_boosts.model.js
@@ -73,10 +73,6 @@ module.exports = sequelize => {
             allowNull: false,
             defaultValue: false,
         },
-        // cronJob: {
-        //     type: DataTypes.STRING,
-        //     allowNull: true, // Should be false but it keeps whining >:(
-        // },
         endAt: {
             type: DataTypes.DATE,
             allowNull: true,
@@ -93,24 +89,7 @@ module.exports = sequelize => {
                     record.time = `0${record.time}`;
                 }
             },
-            // Note doesn't trigger on beforeCreate, could this be a problem elsewhere as well? (cause: createOrUpdateRecord() in route)
-            afterUpsert: async (record, options) => {
-                // Not needed                
-                // // Schedule the redis message and save it
-                // const job = await scheduleBoost(record);
-                // record.cronJob = job.options.name;
-                // await record.save();
-            },
             afterDestroy: (record, options) => {
-                // Not needed
-                // const cronJobName = record.cronJob;
-                // try {
-                //     const cronJob = cron.getTasks().get(cronJobName);
-                //     cronJob.stop()
-                //     cron.getTasks().delete(cronJobName);
-                // } catch (error) {
-                //     console.error(error);
-                // }
                 removedBoost(record);
             },
         },
@@ -125,6 +104,7 @@ module.exports = sequelize => {
             await guild.save({ transaction: t });
         });
         record.isBoostActive = true;
+        record.endAt = new Date(Date.now() + record.duration * 60 * 60 * 1000); // Convert duration from minutes to milliseconds
         await record.save();
         publishMessage(REDIS_CHANNELS.BOOST_SCHEDULE, {
             boostId: record.boostId,
@@ -154,6 +134,7 @@ module.exports = sequelize => {
                 await record.destroy();
             } else {
                 record.isBoostActive = false;
+                record.endAt = null;
                 await record.save();
             }
         } else {
@@ -207,7 +188,7 @@ module.exports = sequelize => {
         const expiredBoosts = await ScheduledBoosts.findAll({
             where: {
                 endAt: {
-                    [Sequelize.Op.lt]: now,
+                    [Sequelize.Op.lt]: new Date(),
                 },
                 isBoostActive: true,
             },
@@ -219,75 +200,10 @@ module.exports = sequelize => {
         } catch (error) {
             console.error(error);
         }
-
     }
 
     // Schedule a single cron job to process all due boosts every minute
     cron.schedule('* * * * *', processBoosts);
-
-    // Little story time:
-    // So I was worried that checking every minute for due boosts would be too much for the server,
-    // so I made a whole thing to schedule the boosts individually with their own cron jobs.
-    // But after reconsidering, I realized that this would be just as if not more taxing on the server.
-    // So now we're seeing what boosts need to be processed every minute.
-
-    // async function scheduleBoost(record, existing = false) {
-    //     const hour = record.time.split(':')[0];
-    //     const minute = record.time.split(':')[1];
-    //     const cronExpression = `${minute} ${hour} * * ${record.day}`;
-
-    //     let job = cron.schedule(cronExpression, async () => {
-    //         const guild = await sequelize.models.guild.findOne({ where: { guildId: record.guildId } });
-    //         const setting = await sequelize.models.guild_settings.findOne({ where: { guildId: record.guildId, type: "scheduled-boost-messages" } });
-
-    //         if (guild) {
-    //             await withTransaction(async (t) => {
-    //                 guild.modifier = record.modifier;
-    //                 guild.duration = record.duration;
-    //                 await guild.save({ transaction: t });
-    //             })
-    //             record.isBoostActive = true;
-    //             await record.save();
-    //             publishMessage(REDIS_CHANNELS.BOOST_SCHEDULE, {
-    //                 guildId: record.guildId,
-    //                 modifier: record.modifier,
-    //                 duration: record.duration,
-    //                 repeat: record.repeat,
-    //                 eventId: record.eventId,
-    //                 channelId: setting.targetId
-    //             });
-    //             if (!record.repeat) {
-    //                 setTimeout(async () => {
-    //                     await record.destroy();
-    //                 }, record.duration * 60 * 1000); // Convert duration from minutes to milliseconds
-    //             } else {
-    //                 setTimeout(async () => {
-    //                     record.isBoostActive = false;
-    //                     await record.save();
-    //                 }, record.duration * 60 * 1000); // Convert duration from minutes to milliseconds
-    //             }
-    //         }
-    //     });
-
-    //     // Update the cronJob field with job.options.name
-    //     try {
-    //         record.cronJob = job.options.name;
-    //         if (existing) {
-    //             await record.save();
-    //             return null
-    //         } else return job
-    //     } catch (error) {
-    //     }
-    // }
-
-    // // Since the cron jobs die when the API dies (or restarts) we need to reinit them all
-    // async function initScheduledBoosts() {
-    //     const records = await ScheduledBoosts.findAll();
-    //     records.forEach(record => {
-    //         scheduleBoost(record, existing=true);
-    //     });
-    // }
-    // initScheduledBoosts()
 
     return ScheduledBoosts;
 };
