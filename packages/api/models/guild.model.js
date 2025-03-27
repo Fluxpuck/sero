dfbdd7ef291cf97aff0393270ec703dc68ec2712
ZakariaX <62657990+ZakariaX1@users.noreply.github.com>
2025-03-27 02:07:33 +0100

    refactor: update expired records logic to reset expireAt field in guild model

--- packages/api/models/guild.model.js
+++ packages/api/models/guild.model.js
@@ -82,7 +82,8 @@ module.exports = sequelize => {
     // Update expired records every 5 minutes
     cron.schedule('*/5 * * * *', async () => {
         try {
-            const result = await Guild.update({ modifier: 1 }, // Note for Flux, maybe also set expireAt = null so it doesn't keep running
+            const result = await Guild.update(
+                { modifier: 1, expireAt: null },
                 {
                     where: {
                         expireAt: {
