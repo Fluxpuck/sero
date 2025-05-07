c2a42838c736fdcad1f8aaa45eb9283e5a72ad25
Mathijs <Mathijsvanderkuijp@live.nl>
2025-05-07 15:13:28 +0200

    wip: model relations

--- packages/api-v2/models/index.ts
+++ packages/api-v2/models/index.ts
@@ -1,12 +1,47 @@
-import { sequelize } from "../database/sequelize";
-import { Circuits } from "../models/circuits.model";
-import { LapTimes } from "../models/laptimes.model";
-import { User } from "../models/users.model";
+import { Sequelize } from "sequelize-typescript";
+import { Guilds } from "./guilds.model";
+import { Users } from "./user.model";
+import { Modifiers } from "./modifiers.model";
 
-sequelize.addModels([Circuits, LapTimes, User]);
+// Export all models
+export { Guilds, Users, Modifiers };
 
-// Define associations
-Circuits.associate();
-LapTimes.associate();
+// Function to initialize models and their relationships
+export const initModels = (sequelize: Sequelize): void => {
+    sequelize.addModels([Guilds, Users, Modifiers]);
 
-export { sequelize, Circuits, LapTimes, User };
+    // Define associations between models
+
+    // Guild associations
+    Guilds.hasMany(Users, {
+        foreignKey: 'guildId',
+        as: 'users'
+    });
+
+    Guilds.hasMany(Modifiers, {
+        foreignKey: 'guildId',
+        as: 'modifiers'
+    });
+
+    // User associations
+    Users.belongsTo(Guilds, {
+        foreignKey: 'guildId',
+        as: 'guild'
+    });
+
+    Users.hasMany(Modifiers, {
+        foreignKey: 'userId',
+        as: 'modifiers'
+    });
+
+    // Modifier associations
+    Modifiers.belongsTo(Guilds, {
+        foreignKey: 'guildId',
+        as: 'guild'
+    });
+
+    Modifiers.belongsTo(Users, {
+        foreignKey: 'userId',
+        as: 'user'
+    });
+};
