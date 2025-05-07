72ef71ec5bef7ff7df2455ebffacca8a54a80c9d
Mathijs <Mathijsvanderkuijp@live.nl>
2025-05-07 15:13:08 +0200

    chore: cleanup sequelize

--- packages/api-v2/database/sequelize.ts
+++ packages/api-v2/database/sequelize.ts
@@ -1,9 +1,9 @@
 import { Sequelize } from "sequelize-typescript";
+import { Guilds, Users } from '../models';
+import { initModels } from '../models';
 
 import { config } from 'dotenv';
 import * as path from 'path';
-
-// Load environment variables from config folder
 config({ path: path.resolve(__dirname, '../config/.env') });
 
 // Setup the environment variables
@@ -12,14 +12,6 @@ const postgres_user: string = process.env.POSTGRES_USER || "postgres";
 const postgres_password: string = process.env.POSTGRES_PASSWORD || "postgres";
 const postgres_db: string = process.env.POSTGRES_DB || "postgres";
 const postgres_port: number = parseInt(process.env.POSTGRES_PORT || "5432", 10);
-
-console.log("postgress", {
-    postgres_host,
-    postgres_user,
-    postgres_password,
-    postgres_db,
-    postgres_port
-})
 
 const sequelize = new Sequelize(
     postgres_db,
@@ -40,8 +32,14 @@ const sequelize = new Sequelize(
         retry: {
             max: 3,
             match: [/Deadlock/i, /Connection acquired timeout/i]
-        }
+        },
+        models: [
+            Guilds,
+            Users,
+        ]
     }
 );
+
+initModels(sequelize); // Initialize model relationships
 
 export { sequelize };
