85d6ed974d9cb0399c11540856372a9e7af463a9
Mathijs <Mathijsvanderkuijp@live.nl>
2025-05-09 21:40:33 +0200

    refactor: chalk seed manager

--- packages/api-v2/database/seeds/index.ts
+++ packages/api-v2/database/seeds/index.ts
@@ -1,41 +1,49 @@
 import { seedGuilds } from './guilds.seed';
 import { seedMessages } from './messages.seed';
 import { sequelize } from '../sequelize';
+import chalk from 'chalk';
 
 (async () => {
-    console.log("Seeding database...");
+    console.log(chalk.blue("Seeding database..."));
 
     // Ensure database connection is established and models are synced
-    await sequelize.authenticate();
-    console.log('Database connection established successfully');
+    try {
+        await sequelize.authenticate();
+        console.log(chalk.green('Database connection established successfully'));
+    } catch (error) {
+        console.error(chalk.red('Error establishing database connection:'), error);
+        process.exit(1);
+    }
 
     // Truncate all tables in the database
     try {
-        console.log('Truncating all tables...');
+        console.log(chalk.yellow('Truncating all tables...'));
         for (const model of Object.values(sequelize.models)) {
             await model.destroy({ truncate: true, cascade: true, force: true });
-            console.log(`Truncated table: ${model.getTableName()}`);
+            console.log(chalk.green(`Truncated table: ${model.getTableName()}`));
         }
+        console.log(chalk.green('All models truncated successfully'));
     } catch (error) {
-        console.error('Error truncating tables:', error);
-        process.exit(1); // Exit process with error code
-    } finally {
-        console.log('All models truncated successfully');
+        console.error(chalk.red('Error truncating tables:'), error);
+        process.exit(1);
     }
 
+    // Seed the database
     try {
+        console.log(chalk.blue('Seeding data...'));
         await seedGuilds();
+        console.log(chalk.green('Guilds seeded successfully'));
         await seedMessages(50);
+        console.log(chalk.green('Messages seeded successfully'));
     } catch (error) {
-        console.error('Error seeding database:', error);
-        process.exit(1); // Exit process with error code
+        console.error(chalk.red('Error seeding database:'), error);
+        process.exit(1);
     } finally {
-        console.log('Seeding completed successfully');
+        console.log(chalk.green('Seeding completed successfully'));
     }
-
-})()
+})();
 
 setTimeout(() => {
-    console.log('Closing process after 10 seconds...');
+    console.log(chalk.magenta('Closing process after 10 seconds...'));
     process.exit(0); // Exit process with success code
 }, 10_000);
