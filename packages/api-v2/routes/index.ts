29c40fde387e83323b40c334dc49e2fdbbe29368
Mathijs <Mathijsvanderkuijp@live.nl>
2025-05-12 17:48:34 +0200

    fix: api author

--- packages/api-v2/routes/index.ts
+++ packages/api-v2/routes/index.ts
@@ -6,9 +6,8 @@ router.get('/', (req: Request, res: Response) => {
     res.status(200).json({
         name: "Sero API",
         version: "2.0.0",
-        authors: ["Mathijs"],
+        authors: ["Fluxpuck"],
         message: 'Successfully connected',
-        documentation: '/docs'
     });
 });
 
