10e8b323b7ced7d858e3d78577b966b072aae1f0
Mathijs <mathijsvanderkuijp@live.nl>
2025-06-06 21:24:50 +0200

    fix: ready economy index route

--- packages/api/routes/guild/[guildId]/economy/index.ts
+++ packages/api/routes/guild/[guildId]/economy/index.ts
@@ -1,6 +1,11 @@
 import { Request, Response, Router, NextFunction } from 'express';
-import { } from '../../../../models';
+import { Op, Transaction } from 'sequelize';
+import { UserBalances } from '../../../../models';
+import { ResponseHandler } from '../../../../utils/response.utils';
+import { ResponseCode } from '../../../../utils/response.types';
 
-const router = Router();
+// Enable mergeParams to access parent route parameters
+const router = Router({ mergeParams: true });
+
 
 export default router;
