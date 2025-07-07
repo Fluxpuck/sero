1a8522ea593606acca76f8b3a13551940f00df6c
Mathijs <Mathijsvanderkuijp@live.nl>
2025-07-07 10:13:37 +0200

    feat: add moderatorSince column on Users

--- packages/api/models/user.model.ts
+++ packages/api/models/user.model.ts
@@ -1,95 +1,108 @@
-import { AfterCreate, Column, DataType, Default, Model, Table } from "sequelize-typescript";
+import {
+  AfterCreate,
+  Column,
+  DataType,
+  Default,
+  Model,
+  Table,
+} from "sequelize-typescript";
 import { UserLevel, UserBalances, Modifier } from "../models";
 
 export enum UserType {
-    ADMIN = "admin",
-    MODERATOR = "moderator",
-    USER = "user"
+  ADMIN = "admin",
+  MODERATOR = "moderator",
+  USER = "user",
 }
 
 @Table({
-    tableName: "users",
-    createdAt: "createdAt",
-    updatedAt: "updatedAt",
-    deletedAt: "deletedAt",
-    paranoid: true,
-    indexes: [
-        {
-            unique: true,
-            fields: ["userId", "guildId"]
-        }
-    ],
-    defaultScope: {
-        attributes: { exclude: ['deletedAt'] }
-    }
+  tableName: "users",
+  createdAt: "createdAt",
+  updatedAt: "updatedAt",
+  deletedAt: "deletedAt",
+  paranoid: true,
+  indexes: [
+    {
+      unique: true,
+      fields: ["userId", "guildId"],
+    },
+  ],
+  defaultScope: {
+    attributes: { exclude: ["deletedAt"] },
+  },
 })
 export class User extends Model<User> {
-    @Column({
-        type: DataType.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-    })
-    declare id: number;
+  @Column({
+    type: DataType.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  declare id: number;
 
-    @Default(DataType.UUIDV4)
-    @Column({
-        type: DataType.UUID,
-        allowNull: false,
-        unique: true,
-    })
-    declare uuid: string;
+  @Default(DataType.UUIDV4)
+  @Column({
+    type: DataType.UUID,
+    allowNull: false,
+    unique: true,
+  })
+  declare uuid: string;
 
-    @Column({
-        type: DataType.STRING,
-        allowNull: false
-    })
-    declare userId: string;
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  declare userId: string;
 
-    @Column({
-        type: DataType.STRING,
-        allowNull: false
-    })
-    declare guildId: string;
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  declare guildId: string;
 
-    @Column({
-        type: DataType.STRING(100),
-        allowNull: false,
-    })
-    declare username: string;
+  @Column({
+    type: DataType.STRING(100),
+    allowNull: false,
+  })
+  declare username: string;
 
-    @Default(false)
-    @Column({
-        type: DataType.BOOLEAN,
-        allowNull: false,
-    })
-    declare premium: boolean;
+  @Default(false)
+  @Column({
+    type: DataType.BOOLEAN,
+    allowNull: false,
+  })
+  declare premium: boolean;
 
-    @Default(() => UserType.USER)
-    @Column({
-        type: DataType.ENUM,
-        values: Object.values(UserType),
-        allowNull: false,
-    })
-    declare userType: UserType;
+  @Default(() => UserType.USER)
+  @Column({
+    type: DataType.ENUM,
+    values: Object.values(UserType),
+    allowNull: false,
+  })
+  declare userType: UserType;
 
-    @AfterCreate
-    static async addModifier(instance: User) {
-        // Run UserLevel and UserBalances upserts in parallel
-        await Promise.all([
-            UserLevel.upsert({
-                guildId: instance.guildId,
-                userId: instance.userId,
-            } as UserLevel),
-            UserBalances.upsert({
-                guildId: instance.guildId,
-                userId: instance.userId,
-            } as UserBalances)
-        ]);
+  @Column({
+    type: DataType.DATE,
+    allowNull: true,
+  })
+  declare moderatorSince: Date | null;
 
-        // Upsert the modifier
-        await Modifier.upsert({
-            guildId: instance.guildId,
-            userId: instance.userId,
-        } as Modifier);
-    }
+  @AfterCreate
+  static async addModifier(instance: User) {
+    // Run UserLevel and UserBalances upserts in parallel
+    await Promise.all([
+      UserLevel.upsert({
+        guildId: instance.guildId,
+        userId: instance.userId,
+      } as UserLevel),
+      UserBalances.upsert({
+        guildId: instance.guildId,
+        userId: instance.userId,
+      } as UserBalances),
+    ]);
+
+    // Upsert the modifier
+    await Modifier.upsert({
+      guildId: instance.guildId,
+      userId: instance.userId,
+    } as Modifier);
+  }
 }
