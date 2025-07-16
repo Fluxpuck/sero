437b1eb2154a4fe49c330ad2d3a096b25a454472
Mathijs <Mathijsvanderkuijp@live.nl>
2025-07-16 12:38:17 +0200

    fix: route on balance-command

--- packages/api/database/seeds/guilds.seed.ts
+++ packages/api/database/seeds/guilds.seed.ts
@@ -1,22 +1,26 @@
-import { Guild } from '../../models';
+import { Guild } from "../../models";
 
-export async function seedGuilds(): Promise<{ success: boolean; error?: unknown; count?: number }> {
-    const guilds = [
-        {
-            guildId: '660103319557111808',
-            guildName: "Fluxpuck's Secret Society",
-            premium: false,
-        },
-    ];
+export async function seedGuilds(): Promise<{
+  success: boolean;
+  error?: unknown;
+  count?: number;
+}> {
+  const guilds = [
+    {
+      guildId: "660103319557111808",
+      guildName: "Fluxpuck's Secret Society",
+      premium: true,
+    },
+  ];
 
-    try {
-        await Guild.bulkCreate(guilds as Guild[], { individualHooks: true });
-        console.log(`${guilds.length} guilds have been processed successfully.`);
-        return { success: true, count: guilds.length };
-
-    } catch (error) {
-        const errorMessage = error instanceof Error ? error.message : 'Unknown error';
-        console.error(`Error seeding guilds: ${errorMessage}`);
-        return { success: false, error };
-    }
+  try {
+    await Guild.bulkCreate(guilds as Guild[], { individualHooks: true });
+    console.log(`${guilds.length} guilds have been processed successfully.`);
+    return { success: true, count: guilds.length };
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : "Unknown error";
+    console.error(`Error seeding guilds: ${errorMessage}`);
+    return { success: false, error };
+  }
 }
